0e96bb23b69978b0cd52732a6904361c4cd0d54f
Dao Quoc Bao <daoquocbao2k04@gmail.com>
2025-11-15 08:22:12 +0700

    Dao Quoc Bao push bai tap 2 - file cpp

---
+++ source.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cout << "nhap n: ";
+cin >> n;
+	if(n >= 0)
+		cout << n << " la so duong";
+	else
+		cout << n << "la so am";
+return 0;
+}

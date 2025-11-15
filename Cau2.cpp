10c5c614f1f573b1942dd4992c2c340eb9f506fd
Thuy Hang <nguyenthuyhang12042004@gmail.com>
2025-11-15 08:23:58 +0700

    Cau2cpp

---
+++ Cau2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Nhap n: ";
+	cin>>n;
+	ktboi3(n);
+	
+	return 0;
+}
+void ktboi3(int n)
+{
+	if(n%3==0)
+		cout<<n<<" la boi cua 3";
+	else
+		cout<<n<<" la boi cua 3";
+}

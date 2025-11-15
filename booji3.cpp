b69314e53d51df9957a18508231d95303ded77fe
lequanghuy <gaterhunter4@gmail.com>
2025-11-15 08:46:52 +0700

    kt boi 3

---
+++ booji3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+bool ktboi3(int x) {
+	if(x % 3 ==0)
+	return true;
+    else
+		return fale;
+}
+int main()
+{
+	int x =9;
+	ktboi3(x);
+	
+	return 0;
+}

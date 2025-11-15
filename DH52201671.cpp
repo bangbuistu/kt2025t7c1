15b169275babc25212e35567a94726018f58a26b
Nguyen Thanh Trung <nttrung2407k4@gmail.com>
2025-11-15 09:04:00 +0700

    chinh code cpp

---
+++ DH52201671.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main() {
+	int n;
+	cout >> "Nhap n:";
+	cin >> n;
+	if (n % 3 == 0)
+		cout >> n " la boi so cua 3";
+	else
+		cout >> n " khong phai boi so cua 3";
+		
+	return 0;
+}
